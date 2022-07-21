103c4fac84c55db184d8d078d729795eaaa95052
kowshik363 <kowshikchowdhury363@gmail.com>
2022-07-21 12:19:01 +0600

    node-backend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,57 @@
+import data from "./data";
+
 function App() {
-  return (
-    <div>
-      <header className="App-header">
-        <a href="/">SMart</a>  
-      </header>
-      <main>List Products</main>
-    </div>
+  return ( <
+    div >
+    <
+    header className = "App-header" >
+    <
+    a href = "/" > SMart < /a>   <
+    /header> <
+    main >
+    <
+    h1 > Featured Products < /h1> <
+    div className = "products" > {
+      data.products.map(product => ( <
+        div className = "product"
+        key = {
+          product.slug
+        } >
+        <
+        a href = {
+          `/product/${product.slug}`
+        } >
+        <
+        img src = {
+          product.image
+        }
+        alt = {
+          product.name
+        }
+        /> <
+        /a> <
+        div className = "product-info" >
+        <
+        a href = {
+          `/product/${product.slug}`
+        } >
+        <
+        p > {
+          product.name
+        } < /p> <
+        /a> <
+        p > < strong > {
+          product.price
+        } < /strong></p >
+        <
+        button > ADD TO CART < /button> <
+        /div> <
+        /div>
+      ))
+    } <
+    /div> <
+    /main> <
+    /div>
   );
 }
 
